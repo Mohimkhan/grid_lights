037bd0c3c5a1910e6f7ec1692e4c3ed85f0cb730
Mohimkhan <mohimkhan652@gmail.com>
2025-02-26 07:48:33 +0600

    Initial Commmit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "../global.css";
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text3xl font-bold underline text-red-700'>
+        Hello React 
+      </h1>
+    </>
+  )
+}
+
+export default App
